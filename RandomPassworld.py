2c28987d978a8d1df8586e5cdb02a20a5d3bc928
Death <lll0Death0lll@gmail.com>
2022-11-15 19:45:08 +0400

    git rm -r --cached .

---
+++ RandomPassworld.py
@@ -0,0 +1,15 @@
+import 
+
+passlen = int(input("Enter password length "))
+passnum = int(input("Enter number of passwords "))
+
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = lower_case.upper()
+numbers = "0123456789"
+symbols = "!@#$%^&*()"
+layout = lower_case+upper_case+numbers+symbols
+
+for i in range(passnum):
+    print(str("".join(random.sample(layout, passlen))))
+
+input()
