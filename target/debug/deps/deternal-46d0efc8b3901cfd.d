2b617a14e52fa301b165704e704a500f24f56458
Death <lll0Death0lll@gmail.com>
2023-01-09 09:41:03 +0400

    Change

---
+++ target/debug/deps/deternal-46d0efc8b3901cfd.d
@@ -0,0 +1,5 @@
+c:\Users\deter\OneDrive\Документы\projects\Deternal\target\debug\deps\deternal-46d0efc8b3901cfd.rmeta: src\main.rs
+
+c:\Users\deter\OneDrive\Документы\projects\Deternal\target\debug\deps\deternal-46d0efc8b3901cfd.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/deternal-df3de0b3e9ef32be.d
@@ -0,0 +1,5 @@
+c:\Users\deter\OneDrive\Документы\projects\Deternal\target\debug\deps\deternal-df3de0b3e9ef32be.rmeta: src\main.rs
+
+c:\Users\deter\OneDrive\Документы\projects\Deternal\target\debug\deps\deternal-df3de0b3e9ef32be.d: src\main.rs
+
+src\main.rs:
